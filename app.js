e87970f758563669d5ed6d638cc76d6324ee517c
Vipul Jaiswal <jaiswalvipul8@gmail.com>
2021-10-22 11:28:44 +0530

    "second commit bcrypt#

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const bcrypt=require("bcrypt");
+const saltingrounds=10;
 
 const app=express();
 
@@ -23,7 +24,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]})
+
 
 const User=new mongoose.model("User",userSchema);
 
@@ -42,17 +43,19 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password,saltingrounds,function(err,hash){
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -66,14 +69,15 @@ app.post("/login",function(req,res){
     }
     else{
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
-        }
-        else{
-          res.send("Password Mismatch");
-        }
-
-      }
+        bcrypt.compare(password,foundUser.password, function(err, result) {
+          if(result===true){
+            res.render("secrets");
+          }
+          else{
+            res.send("Password incorrect");
+          }
+       });
+     }
       else{
         res.send("User not found");
       }
